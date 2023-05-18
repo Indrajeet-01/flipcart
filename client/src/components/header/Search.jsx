1e8bbb49e6bb03ddf62ab4b0a5f81250be6e368b
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-05-18 12:42:48 +0530

    dr

---
+++ client/src/components/header/Search.jsx
@@ -0,0 +1,39 @@
+
+import { InputBase, styled, List,ListItem, Box} from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchContainer = styled(Box)`
+    border-radius: 2px;
+    margin-left: 20px;
+    width: 38%;
+    background-color: #fff;
+    display: flex;
+`;
+const InputSearchBase = styled(InputBase)`
+    font-size: unset;
+    width: 100%;
+    padding-left: 20px;
+    
+`
+const SearchIconWrapper = styled(Box)`
+    margin-left: auto;
+    padding: 5px;
+    display: flex;
+    color: blue;
+`;
+const Search = () => {
+    return (
+        <SearchContainer>
+        <InputSearchBase
+            placeholder="Search for products, brands and more"
+            inputProps={{ 'aria-label': 'search' }}
+        
+        />
+        <SearchIconWrapper>
+        <SearchIcon/>
+        </SearchIconWrapper>
+        </SearchContainer>
+    )
+}
+
+export default Search
