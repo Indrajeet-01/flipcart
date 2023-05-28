b3296e050ac8020530e235994a789cf02a690c9c
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-05-29 01:02:55 +0530

    slide

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import DataProvider from './context/DataProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <DataProvider>
+      <BrowserRouter>
       <Header/>
       <Box style={{marginTop:54}}>
         <Home/>
       </Box>
-      
+      </BrowserRouter>
     </DataProvider>
   );
 }
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -4,6 +4,11 @@ import NavBar from "./NavBar"
 import { useEffect } from "react"
 import { getProducts } from "../../redux/actions/productAction"
 import { useDispatch ,useSelector} from "react-redux"
+import Slide from "./Slide"
+import MidSlide from "./MidSlide"
+import { getProducts as listProducts } from "../../redux/actions/productAction"
+import MidSection from "./MidSection"
+
 
 const Component = styled(Box)`
     padding: 10px;
@@ -11,16 +16,45 @@ const Component = styled(Box)`
 `
 
 const Home = () => {
-    useSelector(state => state.getProducts)
-    const dispatch = useDispatch()
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, error } = getProducts;
+
+    const dispatch = useDispatch();
+
     useEffect(() => {
-        dispatch(getProducts())
-    },[dispatch])
+        dispatch(listProducts())
+    }, [dispatch])
     return (
         <>
             <NavBar/>
             <Component>
                 <Banner/>
+                <MidSlide products={products} />
+                <MidSection/>
+                <Slide
+                    data={products} 
+                    title='Discounts for You'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Suggested Items'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Top Selection'
+                    timer={false} 
+                    multi={true} 
+                />
+                <Slide
+                    data={products} 
+                    title='Recommended Items'
+                    timer={false} 
+                    multi={true} 
+                />
             </Component>
             
         </>
